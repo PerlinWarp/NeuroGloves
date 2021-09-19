9a62f370d4c13fa3d5527a1600c36248b4705269
perlinwarp <perlinwarp@gmail.com>
2021-09-19 15:34:12 +0100

    Updated README

--- Neuro/passthrough.py
+++ Neuro/passthrough.py
@@ -1,17 +1,26 @@
 import serial
+import time
 
 # Arguments
-GLOVE_COM_PORT = "COM8"
+GLOVE_COM_PORT = "COM9" # Check this in windows device manager
 STEAM_COM_PORT = "COM6"
 
 if __name__ == '__main__':
 	glove_ser = serial.Serial(GLOVE_COM_PORT,'115200')  # Serial Port from Glove Arduino
 	steam_ser = serial.Serial(STEAM_COM_PORT,'115200')  # OpenGloves output port
+	time.sleep(3)
+	steam_ser.write(b"\n")
+	steam_ser.flush()
+	glove_ser.flush()
+	steam_ser.flushInput()
+	glove_ser.flushInput()
 
 	while True:
 		try:
 			read = glove_ser.readline()
-			print(read)
+			print(read, type(read))
 			steam_ser.write(read)
 		except KeyboardInterrupt:
+			glove_ser.flushInput()
 			glove_ser.close()
+			steam_ser.close()
